f180fd5ce5a2dfd3aec3a2dfcd6baeb662e179f2
hoangducbach <hoangducbach>
2024-10-16 11:17:49 +0700

    add: layout and base login for form

--- app/(landing)/providers.tsx
+++ app/(landing)/providers.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react";
 
 export default function Providers(props: PropsWithChildren) {
     return (
-        <div>
+        <>
             {props.children}
-        </div>
+        </>
     )
 }
