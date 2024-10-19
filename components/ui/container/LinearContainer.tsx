b53d867245daefbcb9a4eae2e106e541eaa2fab0
HoangDucBach <hoangbach0985@gmail.com>
2024-10-19 22:44:49 +0700

    add markdown editor, support latex

--- components/ui/container/LinearContainer.tsx
+++ components/ui/container/LinearContainer.tsx
@@ -7,6 +7,11 @@ export interface LinearContainerProps extends React.HTMLAttributes<HTMLDivElemen
     direction?: "row" | "column";
     space?: "sm" | "md" | "lg";
     fullWidth?: boolean;
+    label?: string;
+    classNames?: {
+        wrapper?: string;
+        container?: string;
+    };
 }
 export function LinearContainer(props: LinearContainerProps) {
     const {
@@ -15,23 +20,29 @@ export function LinearContainer(props: LinearContainerProps) {
         ...rest
     } = props;
     return (
-        <div
-            {...props}
-            className={clsx(
-                "flex",
-                props.fullWidth && "w-full",
-                direction === "row" && "flex-row",
-                direction === "column" && "flex-col",
-                space === "sm" && "gap-2",
-                space === "md" && "gap-4",
-                space === "lg" && "gap-6",
-                props.isCentered && "justify-center items-center",
-                props.isCenteredX && "justify-center",
-                props.isCenteredY && "items-center",
-                props.className
-            )}
-        >
-            {props.children}
+        <div className={clsx(
+            "flex flex-col gap-2",
+            props.fullWidth && "w-full",
+            props.classNames?.wrapper
+        )}>
+            {props.label && <h3 className="text-sm font-medium text-foreground">{props.label}</h3>}
+            <div
+                {...props}
+                className={clsx(
+                    "flex w-full",
+                    direction === "row" && "flex-row",
+                    direction === "column" && "flex-col",
+                    space === "sm" && "gap-2",
+                    space === "md" && "gap-4",
+                    space === "lg" && "gap-6",
+                    props.isCentered && "justify-center items-center",
+                    props.isCenteredX && "justify-center",
+                    props.isCenteredY && "items-center",
+                    props.classNames?.container
+                )}
+            >
+                {props.children}
+            </div>
         </div>
     )
 }
