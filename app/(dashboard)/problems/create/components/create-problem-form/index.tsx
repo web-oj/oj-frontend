19ceea8c825421ea7b92f4efc026b5e1f2726150
hoangducbach <hoangducbach>
2024-10-25 14:12:08 +0700

    fix: ui for form

--- app/(dashboard)/problems/create/components/create-problem-form/index.tsx
+++ app/(dashboard)/problems/create/components/create-problem-form/index.tsx
@@ -1,29 +1,9 @@
 import { LinearContainer } from "@/components/ui/container/LinearContainer";
 import { CreateProblemForm } from "./CreateProblemForm";
-import { Toolbar } from "@/components/ui";
-import { Switch } from "@nextui-org/switch";
-import { Button } from "@nextui-org/button";
 
 export default function CreateProblemFormWrapper() {
     return (
-        <LinearContainer direction="column" fullWidth>
-            <h1 className="text-3xl font-bold">Create Problem</h1>
-            <Toolbar
-                leftContents={
-                    <>
-                        <Switch
-                            color="secondary"
-                            classNames={{
-                                label: "text-foreground-500",
-                            }} >
-                            View mode
-                        </Switch>
-                    </>
-                }
-                rightContents={
-                    <Button type="submit" form="create-problem-form" color="primary" radius="full">Create</Button>
-                }
-            />
+        <LinearContainer direction="column" fullWidth fullHeight>
             <CreateProblemForm />
         </LinearContainer>
     )
