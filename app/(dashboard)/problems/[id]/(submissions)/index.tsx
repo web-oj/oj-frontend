14050cc25098be0295a8edabd0f6ba965e8a9432
HoangDucBach <hoangbach0985@gmail.com>
2024-10-25 07:27:23 +0700

    fix ui for view

--- app/(dashboard)/problems/[id]/(submissions)/index.tsx
+++ app/(dashboard)/problems/[id]/(submissions)/index.tsx
@@ -2,8 +2,8 @@ import SubmissionsTable from "./components/submissions-table/SubmissionsTable"
 
 export default function Submissions() {
     return (
-        <>
+        <div className="w-full h-full">
             <SubmissionsTable />
-        </>
+        </div>
     )
 }
