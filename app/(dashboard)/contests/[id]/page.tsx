4b9fe395ef31c940f6eb0e8b8534114114e727ff
hoangducbach <hoangducbach>
2024-10-29 00:42:15 +0700

    add: base layout contest

--- app/(dashboard)/contests/[id]/page.tsx
+++ app/(dashboard)/contests/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { LinearContainer, PageContainer } from "@/components/ui";
 import { Splitter, SplitterPanel } from "primereact/splitter";
+import ContestGeneralArea from "./ContestGeneralArea";
+import RankingArea from "./RankingArea";
 
 export default function Home() {
   return (
     <PageContainer>
       <Splitter layout="horizontal" className="w-full h-full space-x-1">
         <SplitterPanel size={60}>
+          <ContestGeneralArea />
         </SplitterPanel>
         <SplitterPanel size={40}>
+          <RankingArea />
         </SplitterPanel>
       </Splitter>
     </PageContainer >
