11de27a1bf758797ea027b1fa15032429f85f22e
hoangducbach <hoangducbach>
2024-10-29 16:56:31 +0700

    add: ui for all problems

---
+++ app/(dashboard)/problems/components/problems-table/index.tsx
@@ -0,0 +1,11 @@
+import ProblemsTable from "./ProblemsTable";
+
+interface ProblemsTableWrapperProps extends React.HTMLAttributes<HTMLDivElement> {
+}
+export function ProblemsTableWrapper(props: ProblemsTableWrapperProps) {
+    return (
+        <>
+            <ProblemsTable />
+        </>
+    )
+}
