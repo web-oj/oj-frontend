3459d7b353c9bd1b8da9ad2641c13ea642f244ab
hoangducbach <hoangducbach>
2024-11-06 10:19:14 +0700

    chore: add layout for leaderboard

---
+++ app/(dashboard)/leaderboard/components/leaderboard-table/data.ts
@@ -0,0 +1,17 @@
+export const columns = [
+    {
+        name: "Standing",
+        uid: "standing",
+        sortable: true,
+    },
+    {
+        name: "Coder",
+        uid: "user",
+        sortable: true,
+    },
+    {
+        name: "Rating",
+        uid: "rating",
+        sortable: true,
+    },
+]
