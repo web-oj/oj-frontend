60326debbd1664d22d13a1f89a81060c1bf17024
HoangDucBach <hoangbach0985@gmail.com>
2024-11-06 17:17:33 +0700

    add: 3 layout for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
+"use client";
 
-import { title, subtitle } from "@/components/primitives";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/dashboard");
+
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
+    <></>
   );
 }
