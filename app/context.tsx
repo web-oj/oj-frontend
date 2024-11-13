d9a50eaabd5d2ec41288f474d070d1ac719ff93b
HoangDucBach <hoangbach0985@gmail.com>
2024-11-13 18:39:02 +0700

    chore: add some component

--- app/context.tsx
+++ app/context.tsx
@@ -1,11 +1,14 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from "react";
 import { User, ObjectContextType } from "@/types";
+import Cookies from "js-cookie";
 
 type AuthContextType = {
     user: User | null;
     login: (token: string) => void;
     logout: () => void;
     loginWithGoogle: () => void;
+    showCookieConsent: boolean;
+    allowCookies: () => void;
 };
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -16,34 +19,45 @@ interface AuthProviderProps {
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
     const [user, setUser] = useState<User | null>(null);
+    const [showCookieConsent, setShowCookieConsent] = useState<boolean>(false);
 
     useEffect(() => {
-        // Check for existing token in local storage
-        const token = localStorage.getItem("token");
+        // Check for existing token in cookies
+        const token = Cookies.get("token");
         if (token) {
             // Validate token and set user
             // This is a placeholder, replace with actual token validation logic
             const decodedUser = JSON.parse(atob(token.split(".")[1])) as User;
             setUser(decodedUser);
         }
+
+        const cookieConsent = Cookies.get("cookieConsent");
+        if (!cookieConsent) {
+            setShowCookieConsent(true);
+        }
     }, []);
 
     const login = (token: string) => {
-        localStorage.setItem("token", token);
+        Cookies.set("token", token);
         const decodedUser = JSON.parse(atob(token.split(".")[1])) as User;
         setUser(decodedUser);
     };
 
     const logout = () => {
-        localStorage.removeItem("token");
+        Cookies.remove("token");
         setUser(null);
     };
 
     const loginWithGoogle = () => {
     };
 
+    const allowCookies = () => {
+        Cookies.set("cookieConsent", "true");
+        setShowCookieConsent(false);
+    };
+
     return (
-        <AuthContext.Provider value={{ user, login, logout, loginWithGoogle }}>
+        <AuthContext.Provider value={{ user, login, logout, loginWithGoogle, showCookieConsent, allowCookies }}>
             {children}
         </AuthContext.Provider>
     );
--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,15 @@
 import { API_URL } from "@/config/constants";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 export const api = axios.create({
     baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+    const token = Cookies.get("token");
+    if (token) {
+        config.headers["x-access-token"] = token;
+    }
+    return config;
 });
