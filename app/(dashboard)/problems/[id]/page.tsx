fbcabf8b596e713a557c536131072c3afbbf40cd
HoangDucBach <hoangbach0985@gmail.com>
2024-11-20 13:46:54 +0700

    add: fetch function problem

--- app/(dashboard)/problems/[id]/page.tsx
+++ app/(dashboard)/problems/[id]/page.tsx
@@ -1,20 +1,44 @@
 import { LinearContainer, PageContainer } from "@/components/ui";
-import ProblemArea from "./components/(tabs)/details";
 import IDEArea from "./IDEArea";
 import { Splitter, SplitterPanel } from "primereact/splitter";
 import ProblemTabs from "./components/(tabs)";
+import { notFound } from "next/navigation";
+import Providers from "./providers";
+import { Problem } from "@/types";
 
-export default function Home() {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+export default async function Page({ params }: Props) {
+  const { id } = params;
+  const fetchProblem = async () => {
+    try {
+      const problem = {} as Problem;
+      return problem;
+    } catch (error) {
+      console.error("Failed to fetch problem", error);
+    }
+  }
+
+  const problem = await fetchProblem();
+  if (!problem) {
+    notFound();
+  }
+
   return (
-    <PageContainer>
-      <Splitter layout="horizontal" className="w-full h-full space-x-1">
-        <SplitterPanel size={60}>
-          <ProblemTabs />
-        </SplitterPanel>
-        <SplitterPanel size={40}>
-          <IDEArea />
-        </SplitterPanel>
-      </Splitter>
-    </PageContainer >
+    <Providers problem={problem}>
+      <PageContainer>
+        <Splitter layout="horizontal" className="w-full h-full space-x-1">
+          <SplitterPanel size={60}>
+            <ProblemTabs />
+          </SplitterPanel>
+          <SplitterPanel size={40}>
+            <IDEArea />
+          </SplitterPanel>
+        </Splitter>
+      </PageContainer >
+    </Providers>
   );
 }
