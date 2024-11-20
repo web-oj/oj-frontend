dc58e0b232bef38c4ed9b5e4b22fdeac2e807f4e
ttb06 <trantuanbinhuet@gmail.com>
2024-11-20 21:37:24 +0700

    add: animate framer motion

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -35,51 +35,64 @@ export const Navbar = () => {
   const { user } = useAuth();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const SearchButton = () => {
+    const [isExpanded, setIsExpanded] = React.useState(false);
 
-  const getIconLink = ({ label }: { label: string }) => {
-    switch (label.toLowerCase()) {
-      case "problems":
-        return <DocumentValidationIcon size={20} className="text-foreground-500" />;
-      case "contests":
-        return <SourceCodeIcon size={20} className="text-foreground-500" />;
-      case "leaderboard":
-        return <RankingIcon size={20} className="text-foreground-500" />;
-      default:
-        return null;
-    }
-  }
+    const handleButtonClick = () => {
+      setIsExpanded(true);
+    };
+
+    const handleBlur = () => {
+      setIsExpanded(false);
+    };
+
+    return (
+      <div className="relative">
+        {isExpanded ? (
+          <Input
+            aria-label="Search"
+            classNames={{
+              inputWrapper: "bg-default-100",
+              input: "text-sm",
+            }}
+            endContent={
+              <Kbd className="hidden lg:inline-block" keys={["command"]}>
+                K
+              </Kbd>
+            }
+            labelPlacement="outside"
+            placeholder="Search..."
+            startContent={
+              <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+            }
+            type="search"
+            autoFocus
+            onBlur={handleBlur}
+          />
+        ) : (
+          <Button
+            variant="light"
+            isIconOnly
+            onClick={handleButtonClick}
+            className="p-2 rounded-full bg-default-100 hover:bg-default-200"
+          >
+            <SearchIcon className="text-base text-default-400" />
+          </Button>
+        )}
+      </div>
+    );
+  };
 
   return (
     <NextUINavbar
       position="sticky"
       classNames={{
-        wrapper: "w-full max-w-full bg-foreground-800 border-b-2 border-divider",
+        wrapper: "w-full max-w-full",
       }}
     >
       <NavbarBrand as="li" className="gap-3 max-w-fit">
         <NextLink className="flex justify-start items-center gap-1" href="/">
-          <Logo />
-          <p className="font-bold text-foreground-100">Sync <span className="text-primary">Up</span></p>
+          <p className="font-bold text-foreground">Sync <span className="text-primary-foreground p-1 rounded-full bg-primary">Up</span></p>
         </NextLink>
       </NavbarBrand>
       <NavbarContent className="basis-1/5 lg:basis-full" justify="start">
@@ -89,27 +102,21 @@ export const Navbar = () => {
               <NavbarItem key={item.href}>
                 <Link
                   className={clsx(
-                    "text-foreground-100",
+                    "text-foreground-500",
                     "flex flex-row items-center gap-1 rounded-full px-3 py-1",
-                    pathname.startsWith(item.href) && "text-primary-foreground bg-primary font-medium"
+                    pathname.startsWith(item.href) && "text-foreground bg-primary font-medium"
                   )}
                   size="sm"
                   as={NextLink}
                   color="foreground"
                   href={item.href}
                 >
-                  {
-                    <div className="w-8 h-8 flex items-center justify-center">
-                      {getIconLink(item)}
-                    </div>
-                  }
                   {item.label}
                 </Link>
               </NavbarItem>
             ))}
           </ul>
           <NavbarMenu className="hidden lg:flex">
-            {searchInput}
             <div className="mx-4 mt-2 flex flex-col gap-2">
               {siteConfig.navMenuItems.map((item, index) => (
                 <NavbarMenuItem key={`${item}-${index}`}>
@@ -130,19 +137,6 @@ export const Navbar = () => {
               ))}
             </div>
           </NavbarMenu>
-          <Search />
-          <ul className="flex gap-4 flex-row w-fit">
-            <Button
-              as={NextLink}
-              href="../ide"
-              className="px-4 w-fit flex flex-row"
-              radius="full"
-              color="secondary"
-              startContent={<KeyboardIcon />}
-            >
-              Open IDE
-            </Button>
-          </ul>
         </div>
       </NavbarContent>
 
@@ -155,23 +149,25 @@ export const Navbar = () => {
             }}
           />
         ) : (
-          <ul className="w-fit hidden lg:flex flex-row gap-4 items-center">
+          <ul className="w-fit hidden lg:flex flex-row gap-1 items-center">
+            <Search />
             <Button
               as={NextLink}
-              href="../register"
+              variant="light"
+              href="../ide"
               radius="full"
-              color="default"
-              className="bg-foreground-700 text-foreground-100"
+              isIconOnly
+              startContent={<KeyboardIcon className="text-foreground-500" />}
             >
-              Register
             </Button>
             <Button
               as={NextLink}
               href="../login"
               radius="full"
               color="primary"
+              className="font-medium shadow-sm shadow-neutral-50"
             >
-              Login
+              Get started
             </Button>
           </ul>
         )
@@ -181,9 +177,6 @@ export const Navbar = () => {
         className="text-foreground-100 lg:hidden"
       />
       <NavbarMenu className="bg-foreground-800">
-        <NavbarMenuItem>
-          {searchInput}
-        </NavbarMenuItem>
         {siteConfig.navItems.map((item) => (
           <NavbarMenuItem key={item.href}>
             <Link
@@ -197,11 +190,6 @@ export const Navbar = () => {
               color="foreground"
               href={item.href}
             >
-              {
-                <div className="w-8 h-8 flex items-center justify-center">
-                  {getIconLink(item)}
-                </div>
-              }
               {item.label}
             </Link>
           </NavbarMenuItem>
@@ -224,21 +212,11 @@ export const Navbar = () => {
             <Button
               fullWidth
               as={NextLink}
-              href="../register"
-              radius="full"
-              color="default"
-              className="bg-foreground-700 text-foreground-100"
-            >
-              Register
-            </Button>
-            <Button
-              fullWidth
-              as={NextLink}
               href="../login"
               radius="full"
               color="primary"
             >
-              Login
+              Get started
             </Button>
           </LinearContainer>
         </NavbarMenuItem>
