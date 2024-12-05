e06cbc1a7decbef5c7947a872c45ccd3187c319a
ttb06 <112877189+ttb06@users.noreply.github.com>
2024-12-05 20:01:55 +0700

    fix: ui for form

--- app/(dashboard)/problems/create/page.tsx
+++ app/(dashboard)/problems/create/page.tsx
@@ -1,10 +1,15 @@
-import { PageContainer } from "@/components/ui";
+import { LinearContainer, PageContainer } from "@/components/ui";
 import CreateProblemFormWrapper from "./components/create-problem-form";
+import FormToolbar from "./components/Toolbar";
 
 export default function Page() {
   return (
     <PageContainer>
-      <CreateProblemFormWrapper />
+      <LinearContainer direction="column" fullWidth fullHeight>
+        <h1 className="text-3xl font-bold">Create Problem</h1>
+        <FormToolbar />
+        <CreateProblemFormWrapper />
+      </LinearContainer>
     </PageContainer>
   );
 }
