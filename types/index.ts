91acb5320748131cd66bf76ea51cb05ef93c7c91
ttb06 <112877189+ttb06@users.noreply.github.com>
2024-12-05 20:01:55 +0700

    fix: fix conflict type from schema

--- types/index.ts
+++ types/index.ts
@@ -49,7 +49,7 @@ export interface Contest {
   end_time: string;
   scoring_rule: string;
   is_plagiarism_check_enabled: boolean;
-  organizer_id: number;
+  organizer_id: string;
   is_published: boolean;
 }
 
@@ -65,6 +65,90 @@ export interface Submission {
   submission_id: number;
   user_id: number;
   problem_id: number;
+  contest_id: number;
+  submitted_at: string;
+  source_code_language: string;
+  source_code_file: string;
+  status: string;
+  compiler_message: string;
+}
+
+export interface Achievement {
+  achievement_id: number;
+  title: string;
+  user_id: number;
+  attachment: string;
+  is_verified: boolean;
+}
+
+export interface Notification {
+  notification_id: number;
+  receive_id: number;
+  content: string;
+  send_at: string;
+}
+
+export interface DiscussionMessage {
+  message_id: number;
+  user_id: number;
+  contest_id: number;
+  problem_id: number;
+  content: string;
+  send_at: string;
+  parent_id?: number;
+}
+
+export interface Tag {
+  tag_id: number;
+  tag_name: string;
+  tag_type: string;
+  is_selected: boolean;
+}
+
+export interface TaggedProblem {
+  tag_id: number;
+  problem_id: number;
+}
+
+export interface PlagiarismReport {
+  report_id: number;
+  submission_id: number;
+  moss_dump_file: string;
+}
+
+export interface SubmissionResult {
+  submission_id: number;
+  test_case_id: number;
+  time_elapsed: number;
+  memory_used: number;
+  output: string;
+  judge_message: string;
+  status: string;
+  judged_at: string;
+}
+
+export interface ContestParticipated {
+  user_id: number;
+  contest_id: number;
+}
+
+export interface ProblemSolved {
+  user_id: number;
+  problem_id: number;
+}
+
+export interface ContestProblem {
+  contest_id: number;
+  problem_id: number;
+  point: number;
+}
+
+export interface Submission {
+  id: string;
+  problem_id: number;
+  source_code_lang: Language | string
+  submission_id: number;
+  user_id: number;
   contest_id: number;
   submitted_at: string;
   source_code_language: string;
