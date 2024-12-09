6e33dafcc8295bf38c7d11f271a5ebc78af53284
ttb06 <trantuanbinhuet@gmail.com>
2024-12-09 23:50:35 +0700

    feat: editor input markdown

---
+++ components/markdown/EditorInputMarkdown.tsx
@@ -0,0 +1,96 @@
+"use client";
+import { Editor } from "@monaco-editor/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Textarea,
+  useDisclosure,
+} from "@nextui-org/react";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import rehypeRaw from "rehype-raw";
+import remarkBreaks from "remark-breaks";
+
+import { LinearContainer } from "@/components/ui/container";
+import "katex/dist/katex.min.css";
+
+export interface EditorInputMarkdownProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  markdown: string;
+  setMarkdown: (markdown: string) => void;
+  register: any;
+}
+const EditorInputMarkdown = React.forwardRef<
+  HTMLDivElement,
+  EditorInputMarkdownProps
+>((props, ref) => {
+  const { markdown, setMarkdown, register, ...rest } = props;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Textarea
+        isRequired
+        required
+        description={
+          <p>
+            Support <b>markdown</b> and <b>latex</b> syntax
+          </p>
+        }
+        label="Statement"
+        labelPlacement="outside"
+        placeholder="Click here to write the statement"
+        radius="full"
+        value={markdown}
+        onClick={onOpen}
+        {...register}
+      />
+      <Modal isOpen={isOpen} size="5xl" onClose={onOpen}>
+        <ModalContent>
+          <ModalHeader>Statement Editor</ModalHeader>
+          <ModalBody>
+            <LinearContainer direction="row">
+              <Editor
+                height={"75vh"}
+                language="markdown"
+                options={{
+                  fontSize: 16,
+                  minimap: {
+                    enabled: false,
+                  },
+                }}
+                theme="light"
+                value={markdown}
+                width={"50%"}
+                onChange={(value) => {
+                  setMarkdown(value as string);
+                }}
+              />
+              <ReactMarkdown
+                rehypePlugins={[rehypeKatex, rehypeRaw]}
+                remarkPlugins={[remarkMath, remarkBreaks]}
+              >
+                {markdown.replace(/&nbsp;/g, "")}
+              </ReactMarkdown>
+            </LinearContainer>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" radius="full" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+});
+
+EditorInputMarkdown.displayName = "EditorInputMarkdown";
+
+export default EditorInputMarkdown;
