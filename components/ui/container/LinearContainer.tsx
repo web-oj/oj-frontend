8cac8d103e0bbd341055962ba1f8aa072b3f8123
HoangDucBach <hoangbach0985@gmail.com>
2024-12-09 23:16:13 +0700

    fix: base ui

--- components/ui/container/LinearContainer.tsx
+++ components/ui/container/LinearContainer.tsx
@@ -37,7 +37,7 @@ export function LinearContainer(props: LinearContainerProps) {
   return (
     <div
       className={twMerge(
-        "flex flex-col gap-2 flex-1",
+        "flex flex-col gap-2 flex-1 h-fit w-fit",
         roundedMedium && "rounded-medium",
         fullWidth && "w-full",
         fullHeight && "h-full",
