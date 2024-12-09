957612fa27da5836ec0bfa29754bd13586a332e9
HoangDucBach <hoangbach0985@gmail.com>
2024-12-09 20:00:35 +0700

    add: edit form

---
+++ app/(dashboard)/problems/edit/[id]/components/Toolbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+import { Switch } from "@nextui-org/react";
+
+import { Toolbar } from "@/components/ui";
+
+export default function FormToolbar() {
+  return (
+    <Toolbar
+      leftContents={
+        <>
+          <Switch
+            classNames={{
+              label: "text-foreground-500",
+            }}
+            color="secondary"
+          >
+            View mode
+          </Switch>
+        </>
+      }
+      rightContents={
+        <Button
+          color="primary"
+          form="update-problem-form"
+          radius="full"
+          type="submit"
+        >
+          Update
+        </Button>
+      }
+    />
+  );
+}
---
+++ app/(dashboard)/problems/edit/[id]/components/fast-check-table/FastCheckTable.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+
+import { useProblem } from "@/app/(dashboard)/problems/context";
+import { LinearContainer } from "@/components/ui";
+import { Field } from "@/components/ui";
+
+interface FastCheckTableProps extends React.HTMLAttributes<HTMLDivElement> { }
+export function FastCheckTable(props: FastCheckTableProps) {
+  const { data } = useProblem();
+
+  const fields = [
+    {
+      label: "Title",
+      value: data ? data.title : "N/A"
+    },
+    {
+      label: "Difficulty",
+      value: data ? data.difficulty : "N/A"
+    },
+    {
+      label: "Time Limit",
+      value: data ? data.timeLimit : "N/A"
+    },
+    {
+      label: "Memory Limit",
+      value: data ? data.memoryLimit : "N/A"
+    }
+  ]
+
+  return (
+    <LinearContainer
+      fullwidth
+      classnames={{
+        wrapper: "w-full bg-foreground-50 rounded-large p-4",
+      }}
+      direction="column"
+      space="sm"
+    >
+      {fields.map((field, index) => (
+        <Field
+          key={index}
+          fullWidth
+          classNames={{
+            wrapper: "justify-between",
+          }}
+          direction="row"
+          label={field.label}
+          value={field.value}
+        />
+      ))}
+    </LinearContainer>
+  );
+}
