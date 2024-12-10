5ee968481f012557992243e5551b56891a7715b8
HoangDucBach <hoangbach0985@gmail.com>
2024-12-10 10:15:14 +0700

    feat: loading page

---
+++ app/loading.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { LinearContainer } from "@/components/ui";
+import { Skeleton } from "@nextui-org/react";
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> { }
+
+export default function Loading(props: Props) {
+    return (
+        <LinearContainer direction="column" isCentered fullheight fullwidth>
+            <Skeleton className="rounded-2xl bg-foreground-200 w-full h-16" />
+            <LinearContainer isCentered fullheight fullwidth>
+                <Skeleton className="flex-1 rounded-2xl bg-foreground-200 w-full h-full" />
+                <Skeleton className="flex-1 rounded-2xl bg-foreground-200 w-full h-full" />
+            </LinearContainer>
+        </LinearContainer>
+    )
+}
