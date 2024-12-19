4406937497d36d225cfe6a972d806c2f20fd8d3d
ttb06 <trantuanbinhuet@gmail.com>
2024-12-19 11:55:23 +0700

    fix: api

--- fetch-functions/index.ts
+++ fetch-functions/index.ts
@@ -12,7 +12,7 @@ import { api } from "@/utils/api";
 export async function signUp(params: {
   email: string;
   password: string;
-  handle: string; // username
+  handle: string; // handle
 }) {
   try {
     const res = await api.post<ApiResponse<User>>("/user", params);
@@ -26,7 +26,10 @@ export async function signUp(params: {
 
 export async function login(params: { email: string; password: string }) {
   try {
-    const res = await api.post<ApiResponse<{ token: string }>>("/user/login", params);
+    const res = await api.post<ApiResponse<{ token: string }>>(
+      "/user/login",
+      params,
+    );
 
     return res.data.data.token;
   } catch (error) {
@@ -71,7 +74,9 @@ export async function getUserByHandle(params: {
   handle: string;
 }): Promise<User> {
   try {
-    const res = await api.get<ApiResponse<User>>(`/user/handle/${params.handle}`);
+    const res = await api.get<ApiResponse<User>>(
+      `/user/handle/${params.handle}`,
+    );
 
     return res.data.data;
   } catch (error) {
@@ -106,13 +111,10 @@ export async function updateUser(params: Partial<User>) {
 */
 
 export async function createProblem(params: {
+  isPublished: Problem["isPublished"];
   title: Problem["title"];
   difficulty: Problem["difficulty"];
   statement: Problem["statement"];
-  /**
-   * @ignore: tags are not implemented yet
-   */
-  // tags: string[];
   timeLimit: Problem["timeLimit"];
   memoryLimit: Problem["memoryLimit"];
   inputFormat: Problem["inputFormat"];
@@ -158,10 +160,13 @@ export async function addTestcaseToProblem(params: {
   output: string;
 }) {
   try {
-    const res = await api.post<ApiResponse<null>>(`/problem/${params.problemId}/testcase`, {
-      input: params.input,
-      output: params.output,
-    });
+    const res = await api.post<ApiResponse<null>>(
+      `/problem/${params.problemId}/testcase`,
+      {
+        input: params.input,
+        output: params.output,
+      },
+    );
 
     return res.data;
   } catch (error) {
@@ -174,9 +179,12 @@ export async function addTestcasesToProblem(params: {
   testcases: { input: string; output: string }[];
 }) {
   try {
-    const res = await api.post<ApiResponse<null>>(`/problem/${params.problemId}/testcases`, {
-      testcases: params.testcases,
-    });
+    const res = await api.post<ApiResponse<null>>(
+      `/problem/${params.problemId}/testcases`,
+      {
+        testcases: params.testcases,
+      },
+    );
 
     return res.data;
   } catch (error) {
@@ -189,7 +197,9 @@ export async function getProblemByTitle(params: {
   title: string;
 }): Promise<Problem[]> {
   try {
-    const res = await api.get<ApiResponse<Problem[]>>(`/problem/title/${params.title}`);
+    const res = await api.get<ApiResponse<Problem[]>>(
+      `/problem/title/${params.title}`,
+    );
 
     return res.data.data;
   } catch (error) {
@@ -221,11 +231,11 @@ export async function getAllProblems(
 }
 
 export async function searchProblems(params: {
-  searchKeyword?: string,
-  difficultyLow?: number,
-  difficultyHigh?: number,
-  offset?: number,
-  limit?: number,
+  searchKeyword?: string;
+  difficultyLow?: number;
+  difficultyHigh?: number;
+  offset?: number;
+  limit?: number;
 }): Promise<Problem[]> {
   const {
     searchKeyword = "",
@@ -284,6 +294,7 @@ export async function getContestById(params: { id: number }) {
 
     return res.data.data;
   } catch (error) {
+    console.error(error);
     throw new Error("Failed to get contest by ID");
   }
 }
@@ -327,7 +338,10 @@ export async function updateContest(params: {
   data: Partial<Contest>;
 }) {
   try {
-    const res = await api.patch<ApiResponse<null>>(`/contest/${params.id}`, params.data);
+    const res = await api.patch<ApiResponse<null>>(
+      `/contest/${params.id}`,
+      params.data,
+    );
 
     return res.data;
   } catch (error) {
@@ -385,30 +399,35 @@ export async function addProblemToContest(params: {
 
     return res.data;
   } catch (error) {
+    console.error(error);
     throw new Error("Failed to add problem to contest");
   }
 }
 
 export async function removeProblemFromContest(params: {
-  contestId: number;
+  id: number;
   problemId: number;
 }) {
   try {
-    const res = await api.delete<ApiResponse<null>>(`/contest/${params.contestId}/problem`, {
+    const res = await api.delete<ApiResponse<null>>(`/contest/${params.id}/problem`, {
       data: {
         problemId: params.problemId,
       },
-    });
+    }
+    );
 
     return res.data;
   } catch (error) {
+    console.error(error);
     throw new Error("Failed to remove problem from contest");
   }
 }
 
-export async function getProblemsInContest(params: { contestId: number }) {
+export async function getProblemsInContest(params: { id: number }) {
   try {
-    const res = await api.get<ApiResponse<Problem[]>>(`/contest/${params.contestId}/problem`);
+    const res = await api.get<ApiResponse<Problem[]>>(
+      `/contest/${params.id}/problem`,
+    );
 
     return res.data.data;
   } catch (error) {
@@ -418,12 +437,15 @@ export async function getProblemsInContest(params: { contestId: number }) {
 
 export async function registerForContest(params: {
   userId: number;
-  contestId: number;
+  id: number;
 }) {
   try {
-    const res = await api.post<ApiResponse<Contest>>(`/contest/${params.contestId}/register`, {
-      id: params.userId,
-    });
+    const res = await api.post<ApiResponse<Contest>>(
+      `/contest/${params.id}/register`,
+      {
+        id: params.userId,
+      },
+    );
 
     return res.data.data;
   } catch (error) {
@@ -433,14 +455,17 @@ export async function registerForContest(params: {
 
 export async function unregisterForContest(params: {
   userId: number;
-  contestId: number;
+  id: number;
 }) {
   try {
-    const res = await api.delete<ApiResponse<null>>(`/contest/${params.contestId}/register`, {
-      data: {
-        id: params.userId,
+    const res = await api.delete<ApiResponse<null>>(
+      `/contest/${params.id}/register`,
+      {
+        data: {
+          id: params.userId,
+        },
       },
-    });
+    );
 
     return res.data;
   } catch (error) {
@@ -467,7 +492,7 @@ export async function runMoss(params: {
 */
 export async function createSubmission(params: {
   problemId: number;
-  contestId?: number;
+  id?: number;
   code: string;
 }) {
   // code must be base64 encoded, if not, encode it
@@ -482,6 +507,7 @@ export async function createSubmission(params: {
 
     return res.data;
   } catch (error) {
+    console.error(error);
     throw new Error("Failed to create submission");
   }
 }
