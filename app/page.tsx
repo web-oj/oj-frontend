3be445696d1dbf8e7fa5f6d56d7519d92d0f9ea5
HoangDucBach <hoangbach0985@gmail.com>
2024-12-20 10:09:20 +0700

    Release new git add .!

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import { PageContainer } from "@/components/ui";
 import { Hero } from "./components/Hero";
 
 export default function Home() {
-
   return <PageContainer isCentered>
     <Hero />
   </PageContainer>;
